fb3397ef3dd0991c6843a3da80cb49607c80d4e9
Joelle Carbonell <nda108252@gmail.com>
2025-08-22 19:47:19 +0000

    recipe dom references

--- src/main/resources/public/frontend/recipe/recipe-page.js
+++ src/main/resources/public/frontend/recipe/recipe-page.js
@@ -16,6 +16,26 @@ window.addEventListener("DOMContentLoaded", () => {
      * - Admin link and logout button
      * - Search input
     */
+    const logoutButton = document.getElementById("logout-button");
+    const adminLink = document.getElementById("admin-link");
+    const recipeList = document.getElementById("recipe-list");
+
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
+
+    const addName = document.getElementById("add-recipe-name-input");
+    const addInstructions = document.getElementById("add-recipe-instructions-input");
+    const addButton = document.getElementById("add-recipe-submit-input");
+
+    const updateName = document.getElementById("update-recipe-name-input");
+    const updateInstructions = document.getElementById("update-recipe-instructions-input");
+    const updateButton = document.getElementById("update-recipe-submit-input");
+
+    const deleteName = document.getElementById("delete-recipe-name-input");
+    const deleteButton = document.getElementById("delete-recipe-submit-input");
+
+    const token = sessionStorage.getItem("auth-token");
+    const isAdmin = sessionStorage.getItem("isAdmin");
 
     /*
      * TODO: Show logout button if auth-token exists in sessionStorage
